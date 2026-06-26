098e7c6d11ad2d355b9dffeb7fbaca9b1367cd12
Manzar <manzarnazar@gmail.com>
2026-06-26 12:01:12 +0100

    first commit

---
+++ config/system-addons.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'sandrojvs',
+    'purchase_key' => 'b1dbf155-9c2d-45c4-b1c9-87b0930f147a',
+    'software_id' => 'MzY3NzIxMTI=',
+    'domain' => 'admin.b2you.co.uk',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'MzY3NzIxNzM=',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'MzY3NzIxNDg=',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'react_web' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'NDUzNzAzNTE=',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
